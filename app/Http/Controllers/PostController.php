7b622e7559282825e0d383bcc8a69b27fd0a3df5
Vandana <vandana.k.kacha87@gmail.com>
2025-08-04 16:02:12 +0530

    Store Laravel

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::orderBy('title', 'desc')->paginate(5);
+        $posts = Post::orderBy('title', 'desc')->paginate(2);
         return view('posts.index', compact('posts'));
     }
 
